45203b773056dfa918829ab1aaa468d28543ec02
Balasekar <arun_mohan_raj.balasekar.ext@nokia.com>
2020-05-09 22:22:03 +0530

    added ifttt mobile notification

--- nse_collect.py
+++ nse_collect.py
@@ -9,7 +9,14 @@ import json
 toaster = ToastNotifier()
 
 
-def stock_scraper(url, name, threshold, operation):
+def notify_mobile(name, price, url, ifttt_key, ifttt_event):
+    iftttUrl = 'https://maker.ifttt.com/trigger/'+ifttt_event+'/with/key/'+ifttt_key
+    myobj = {"value1": name,"value2": price,"value3": url}
+    x = requests.post(iftttUrl, data=myobj)
+    print(x.text)
+
+
+def stock_scraper(url, name, threshold, operation, ifttt_key, ifttt_event):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     result = soup.select(
@@ -19,17 +26,20 @@ def stock_scraper(url, name, threshold, operation):
     if operation == 'gt':
         if price > threshold:
             toaster.show_toast(name+" share price", str(price), icon_path=None, duration=5)
+            notify_mobile(name, price, url, ifttt_key, ifttt_event)
     if operation == 'lt':
         if price < threshold:
             toaster.show_toast(name+" share price", str(price), icon_path=None, duration=5)
+            notify_mobile(name, price, url, ifttt_key, ifttt_event)
 
 def get_all_stock_info():
     with open('./input.json') as f:
         data = json.load(f)
-
+    ifttt_key = data['iftttKey']
+    ifttt_event = data['iftttEvent']
     for stock in data['stocks']:
         print(stock['name'])
-        stock_scraper(stock['url'], stock['name'], stock['threshold'], stock['operation'])
+        stock_scraper(stock['url'], stock['name'], stock['threshold'], stock['operation'], ifttt_key, ifttt_event)
 
 
 
